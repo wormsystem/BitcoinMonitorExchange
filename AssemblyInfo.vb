581d7412fed12360efb6af22ed60692e3cfd2d72
wormsystem <wormsystemhack@gmail.com>
2016-02-28 10:12:37 -0300

    Added files via upload

---
+++ AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Informações gerais sobre um assembly são controladas através do seguinte 
+' conjunto de atributos. Altere o valor destes atributos para modificar a informação
+' associada a um assembly.
+
+' Revise os valores dos atributos do assembly
+
+<Assembly: AssemblyTitle("Bitcoin Monitor")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Worm System")> 
+<Assembly: AssemblyProduct("Bitcoin Monitor")> 
+<Assembly: AssemblyCopyright("Copyright Worm System")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'O GUID a seguir é para o ID da typelib se este projeto for exposto para COM
+<Assembly: Guid("f8f0f0a8-0985-438d-816e-bad233e03acd")> 
+
+' Informação de versão para um assembly consiste nos quatro valores a seguir:
+'
+'      Versão Principal
+'      Versão Secundária 
+'      Número da Versão
+'      Revisão
+'
+' É possível especificar todos os valores ou usar o padrão de números da Compilação e de Revisão 
+' utilizando o '*' como mostrado abaixo:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.1.2.0")> 
+<Assembly: AssemblyFileVersion("1.1.2.0")> 
